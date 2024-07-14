114085d070dbe8b242cab83170434447eb9cc10f
qingchen <qingchen@tesla.com>
2024-07-13 19:36:54 -0700

    Update sign.py

--- sign.py
+++ sign.py
@@ -7,12 +7,11 @@ def sign(m):
     w3 = Web3()
     # create an eth account and recover the address (derived from the public key) and private key
     # your code here
-
-    eth_address = '0x3b7f6e8011Bc2137ea1Aca77fAeDbe84Af46374A'  # Eth account
-    private_key = 'b3d8146f623407e7691479caeefb6332b60665ad9fee90a8697feaac79b783b0'
-
-    # Correct way to create an account using the private key
     account = eth_account.Account.from_key(private_key)
+
+    eth_address = account.address
+    #eth_address = '0x3b7f6e8011Bc2137ea1Aca77fAeDbe84Af46374A'  # Eth account
+    private_key = 'b3d8146f623407e7691479caeefb6332b60665ad9fee90a8697feaac79b783b0'    
 
     # Generate the message to be signed
     message = encode_defunct(text=m)
