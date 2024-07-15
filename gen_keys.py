9598abf7bcd1fd7ee35da8d7831b122fb4b75fba
qingchen <qingchen@tesla.com>
2024-07-15 09:37:39 -0700

    Update gen_keys.py

--- gen_keys.py
+++ gen_keys.py
@@ -2,6 +2,7 @@ from web3 import Web3
 import eth_account
 import os
 from eth_account import Account
+
 def get_keys(challenge,keyId = 0, filename = "eth_mnemonic.txt"):
     """
     Generate a stable private key
